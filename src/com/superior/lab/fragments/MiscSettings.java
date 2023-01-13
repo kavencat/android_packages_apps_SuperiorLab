eafbba9e48fbae5b9d5d16d200861d9fbc38efa9
aswin7469 <aswinas@pixysos.com>
2023-01-13 20:25:48 +0530

    SuperiorLab: Add Toggle for GamesProp and google photos spoof
    Change-Id: Id2c0f8f4674ecfe75dede1a2bfee7736f8273ac5
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- src/com/superior/lab/fragments/MiscSettings.java
+++ src/com/superior/lab/fragments/MiscSettings.java
@@ -34,6 +34,7 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 import android.provider.Settings;
 import com.android.settings.R;
 
@@ -45,17 +46,52 @@ import com.android.settings.SettingsPreferenceFragment;
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
+    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+
+    private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
+    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+
+    private SwitchPreferenceCompat mGamesSpoof;
+    private SwitchPreferenceCompat mPhotosSpoof;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         addPreferencesFromResource(R.xml.superior_lab_misc);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        final Resources res = getResources();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mGamesSpoof = (SwitchPreferenceCompat) prefScreen.findPreference(KEY_GAMES_SPOOF);
+        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
+        mGamesSpoof.setOnPreferenceChangeListener(this);
+
+        mPhotosSpoof = (SwitchPreferenceCompat) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
+        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
+        mPhotosSpoof.setOnPreferenceChangeListener(this);
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        SystemProperties.set(SYS_GAMES_SPOOF, "false");
+        SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
 
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+    	Context mContext = getActivity().getApplicationContext();
+	ContentResolver resolver = mContext.getContentResolver();
+        if (preference == mGamesSpoof) {
+            boolean value = (Boolean) objValue;
+            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            return true;
+        } else if (preference == mPhotosSpoof) {
+            boolean value = (Boolean) objValue;
+            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
+            return true;
+        }
         return false;
     }
 
